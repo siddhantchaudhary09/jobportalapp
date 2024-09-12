39275b7d5a3c7a03006ac6c7344c10ebaf1ff41d
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-09-13 01:52:10 +0530

    bug-fix

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,4 +1,8 @@
-export const USER_API_ENDPOINT="https://job-portal-ten-sable.vercel.app/api/v1/user";
-export const JOB_API_ENDPOINT="https://job-portal-ten-sable.vercel.app/api/v1/job"
-export const APPLICATION_API_ENDPOINT="https://job-portal-ten-sable.vercel.app/api/v1/application"
-export const COMPANY_API_ENDPOINT="https://job-portal-ten-sable.vercel.app/api/v1/company"
+export const USER_API_ENDPOINT =
+  "https://jobportalapp-ovlm.vercel.app/api/v1/user";
+export const JOB_API_ENDPOINT =
+  "https://jobportalapp-ovlm.vercel.app/api/v1/job";
+export const APPLICATION_API_ENDPOINT =
+  "https://jobportalapp-ovlm.vercel.app/api/v1/application";
+export const COMPANY_API_ENDPOINT =
+  "https://jobportalapp-ovlm.vercel.app//v1/company";
