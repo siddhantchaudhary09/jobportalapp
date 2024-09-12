e063987f1bd9c2899ecab01c7e2ed323d3794dc1
siddhantchaudhary09 <siddhantchaudhary096@gmail.com>
2024-09-13 01:59:28 +0530

    bug-fixes-2

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -4,5 +4,6 @@ export const JOB_API_ENDPOINT =
   "https://jobportalapp-ovlm.vercel.app/api/v1/job";
 export const APPLICATION_API_ENDPOINT =
   "https://jobportalapp-ovlm.vercel.app/api/v1/application";
+
 export const COMPANY_API_ENDPOINT =
   "https://jobportalapp-ovlm.vercel.app//v1/company";
